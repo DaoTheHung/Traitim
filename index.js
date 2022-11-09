765118c3c9bb538bf5e9cbdd0697a2fbb41be417
DaoTheHung <Daohung447@gmail.com>
2022-11-09 21:16:57 +0700

    landau

---
+++ index.js
@@ -0,0 +1,23 @@
+// // bài 7.8
+// let arrStudent = ['Lan', 'Điệp', 'Ngọc', 'Ngạn', 'Trinh'];
+// // arrStudent.splice(4,5);
+// arrStudent[0] = 'Phu'
+// console.log(arrStudent);
+
+// let languages = [
+//     'Javacript',
+//     'PHP',
+//     'Ruby'
+// ];
+// languages.splice(1, 0, 'Dả');
+// console.log(languages)
+
+// let stringHTML = '';
+//   for (let i = 0; i <= 99; i++) {
+    
+// }
+// console.log(i);
+
+//     stringHTML += `<li> ${i} </li>`;
+//   }
+//   document.getElementById('name').innerHTML = stringHTML;
